7b20cdd139ce5c6367c19835cc1f7018ca914bdf
unknown <andresmaram1997@gmail.com>
2023-08-11 19:43:35 -0400

    Final version keeper styled

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
+import Fab from '@mui/material/Fab';
 
 function CreateArea(props){
 
@@ -6,6 +9,8 @@ function CreateArea(props){
         title:"",
         content:""
     });
+
+    const [expand, setExpand] = useState(false);
 
     function handleChange(event){
 
@@ -25,12 +30,20 @@ function CreateArea(props){
         setEntry({title:"", content: ""});
     }
 
+    function handleClick(){
+        setExpand(!expand);
+    }
+
     return (
         <div>
-            <form>
-                <input onChange={handleChange} value={entry.title} name="title" placeholder="Title..."/>
-                <textarea onChange={handleChange} value={entry.content} name="content" placeholder="Take a note..."/>
-                <button onClick={handleAdd}>Add</button>
+            <form className="create-note">
+                { expand && <input onChange={handleChange} value={entry.title} name="title" placeholder="Title..."/>}
+                <textarea onClick={handleClick} rows={expand ? "3" : "1"} onChange={handleChange} value={entry.content} name="content" placeholder="Take a note..."/>
+                <Zoom in={expand}>
+                    <Fab onClick={handleAdd}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props){
 
@@ -11,7 +12,9 @@ function Note(props){
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onClick={onDelete} >Delete</button>
+            <button onClick={onDelete} >
+                <DeleteIcon />
+                </button>
         </div>
     )
 }
